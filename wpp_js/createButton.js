6a315d27d30ab05411403b384d438c6e10b66ae0
Alexsander Alves <alvesalexsander@live.com>
2019-06-26 08:48:33 -0300

    implementado sendMessage()

--- wpp_js/createButton.js
+++ wpp_js/createButton.js
@@ -123,6 +123,15 @@ class WhatsappButton {
 
         return mounted;
     }
+
+    sendMessage(){
+        var userInput = document.getElementById("message").value;
+        userInput = encodeURIComponent(userInput.trim());
+        var apiURL = "https://api.whatsapp.com/send?phone=5522997055388&text=" + userInput;
+        window.open(apiURL, "height=200", "width=200");
+        document.getElementById("message").value = "";
+        return false;
+    }
     
 }
 
