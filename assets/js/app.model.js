a0e92800572033af323c7f37a0b348fd2d2c1afb
Alexsander Alves <alvesalexsander@live.com>
2019-08-04 03:34:38 -0300

    Correções e melhorias

--- assets/js/app.model.js
+++ assets/js/app.model.js
@@ -17,7 +17,7 @@ export default class App extends FontShifter{
         this.jsCode = ''
         this.setTitleBackground()
         this.setTitleFont()
-        this.resourcesURL = 'https://sashaclimax.github.io/whatsappButton/whatsappButton.js/dist/'
+        this.resourcesURL = 'https://sashaclimax.github.io/whatsappButton.js/dist/'
     }
 
     validatePhoneNumber(phoneNumber){
