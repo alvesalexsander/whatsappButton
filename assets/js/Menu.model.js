5c9bbd48c619ca1b7c4ccf692fff50388c9f1173
Alexsander Alves <alvesalexsander@live.com>
2019-11-08 10:35:48 -0300

    Correções
    Corrigida a URL do github
    sashaclimax -> alvesalexsander

--- assets/js/Menu.model.js
+++ assets/js/Menu.model.js
@@ -1,9 +1,9 @@
 export default class Menu{
 
     aboutContent = "whatsappButton.js is a front-end plugin that comes to help developers implement a communication channel using the WhatsApp Web API for their clients.<br><br>It is easy to deploy and highly customizable!"
-    documentationContent = "Documentation explains more about the features and general usage of the plugin.<br><br>It also contains details of the plugin's building process.<br><br>Check it out <a href='https://github.com/sashaclimax/whatsappButton'>here!</a>"
+    documentationContent = "Documentation explains more about the features and general usage of the plugin.<br><br>It also contains details of the plugin's building process.<br><br>Check it out <a href='https://github.com/alvesalexsander/whatsappButton'>here!</a>"
     creditsContent = "I'd like to thank everyone that helped on this plugin and the developers/designers of the amazing resources I used on this project:<ul><li><a href='http://jscolor.com/'>jscolor</a> by Jan Odvárko – East Desire</li><li><a href='https://www.flaticon.com/authors/google'>SVG icons</a> by Google</li><li><a href='https://www.flaticon.com/authors/freepik'>SVG icons</a> by Freepik</li><li><a href='https://www.flaticon.com/authors/smashicons'>SVG icons</a> by Smashicons</li><li><a href='https://www.flaticon.com/authors/pixel-perfect'>SVG icons</a> by Pixel perfect</li></ul>"
-    contactContent = "Hello! My name is Alexsander Alves. I am the developer behind whatsappButton.js.<br><br>Here are some contacts where you can find me:<br>E-mail: alvesalexsander@live.com<br>Github: <a href='https://github.com/sashaclimax'>https://github.com/sashaclimax</a>"
+    contactContent = "Hello! My name is Alexsander Alves. I am the developer behind whatsappButton.js.<br><br>Here are some contacts where you can find me:<br>E-mail: alvesalexsander@live.com<br>Github: <a href='https://github.com/alvesalexsander'>https://github.com/alvesalexsander</a>"
 
     constructor(){
     }
